a4c3bd943525a74c9ba77bbdbac5979513284545
Dustin Huntsberger <dhunts258@gmail.com>
2022-07-11 13:50:59 -0400

    add more style and add framework for showcase page

--- src/components/Showcase/index.js
+++ src/components/Showcase/index.js
@@ -1,6 +1,29 @@
+import data from '../../assets/showcase/projects.json';
+const projects = data.projects;
+
 function Showcase() {
     return(
-        <div>Hello World</div>
+        <div>
+        {projects.map((project, i) =>  (
+            <div className="showcase-el" style={{ backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${require(`../../assets/showcase/img/${project.img_path}`)})` }} key={i}>
+            <div className="showcase-title">
+                <h3>{project.title}</h3>
+                <p>{project.skills}</p>
+            </div>
+            <div className="showcase-body">
+                <div>
+                    <p>{project.description}</p>
+                    <a href={project.github_link} rel="noreferrer" target="_blank" className="btn">
+                        View on Github
+                    </a>
+                    <a href={project.deployed_link} rel="noreferrer" target="_blank" className="btn">
+                        View Deployed
+                    </a>
+                </div>
+            </div>
+        </div>
+        ))}
+        </div>
     )
 }
 
