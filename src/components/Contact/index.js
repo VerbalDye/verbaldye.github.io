ba221ece96a6546cf0091e35afbbe89f1ad5c43e
Dustin Huntsberger <dhunts258@gmail.com>
2022-07-11 15:53:08 -0400

    style projects section

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,23 @@
 function Contact() {
-    return(
-        <div>Hello World2</div>
+    return (
+        <div>
+            <h2 className="section-title">Get in Touch</h2>
+            <p className="contact-text">Thanks for visiting the site! If you are thinking about reaching out, listed below is my contact information. Looking forward to anyone reaching out!</p>
+            <ul className="contact">
+                <li>
+                    <a href="tel:3307494962">330-749-4962</a>
+                </li>
+                <li>
+                    <a href="mailto:dhunts258@gmail.com">dhunts258@gmail.com</a>
+                </li>
+                <li>
+                    <a href="https://github.com/VerbalDye">Github</a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/VerbalDye">Twitter</a>
+                </li>
+            </ul>
+        </div>
     )
 }
 
--- src/components/Showcase/index.js
+++ src/components/Showcase/index.js
@@ -2,17 +2,16 @@ import data from '../../assets/showcase/projects.json';
 const projects = data.projects;
 
 function Showcase() {
+
+
     return (
-        <div>
-            {projects.map((project, i) => (
+        <div className='showcase'>
+            <h2 class="section-title">My Work</h2>
+            {projects.reverse().map((project, i) => (
                 <div className="showcase-el" style={{ backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${require(`../../assets/showcase/img/${project.img_path}`)})` }} key={i}>
-                    <div className="showcase-title">
-                        <h3>{project.title}</h3>
-                        <p>{project.skills}</p>
-                    </div>
                     <div className="showcase-body">
-                        <div>
-                            <p>{project.description}</p>
+                        <p>{project.description}</p>
+                        <div className="showcase-btn">
                             <a href={project.github_link} rel="noreferrer" target="_blank" className="btn">
                                 View on Github
                             </a>
@@ -21,6 +20,10 @@ function Showcase() {
                                     View Deployed
                                 </a>}
                         </div>
+                    </div>
+                    <div className="showcase-title">
+                        <h3>{project.title}</h3>
+                        <p>{project.skills}</p>
                     </div>
                 </div>
             ))}
